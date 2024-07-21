a2e42c93fff9e6f58d0197e25fe859e7ed49027b
abdull-m <abdullyhosh@gmail.com>
2024-07-20 23:42:07 -0500

    Remove .DS_Store files and update .gitignore

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,53 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Portfolio {
+    @Id
+    @GeneratedValue
+    private long portfolioId;
+
+    @ManyToOne
+    @JoinColumn(name = "clientId",nullable = false)
+    private Client client;
+
+    @Column(nullable = false)
+    private String creationDate;
+
+    @OneToMany(mappedBy = "portfolio")
+    private List<Security> securities;
+
+    public Portfolio(){
+
+    }
+
+    public Portfolio(Client client, String creationDate) {
+        this.client = client;
+        this.creationDate = creationDate;
+    }
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+}
